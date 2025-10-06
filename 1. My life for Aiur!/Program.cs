d004e5dc48c3eff39bfa6d05f733875ee854d9ca
Kawaii8192 <mosupet128@gmail.com>
2025-10-06 17:28:58 +0900

    fixed untracked files

---
+++ 1. My life for Aiur!/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Fenix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My life for Aiur!");
+        }
+    }
+}
+//I hate Zerg.
